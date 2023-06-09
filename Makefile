c098a8153e46cd32691ddc5df72703a13d791e53
Youssef Tijani <ytijani@e1r11p11.1337.ma>
2023-06-09 12:57:52 +0100

    ADD makefile

--- Makefile
+++ Makefile
@@ -1,8 +1,23 @@
-up: docker-compose up
-down : docker-compose down
-build: docker-compose build
-clean : docker-compose down -v -remove-orphans
-re : clean
-	docker-compose up --build
+
+COMPOSE_FILE = ./srcs/docker-compose.yml
+
+all: up
+
+up:
+	@docker-compose -f $(COMPOSE_FILE) up
+
+down:
+	@docker-compose -f $(COMPOSE_FILE) down
+
+build:
+	@docker-compose -f $(COMPOSE_FILE) build
+
+clean:
+	@docker-compose -f $(COMPOSE_FILE) down -v
+
+fclean: clean
+	@docker system prune -a
+re: clean
+	@docker-compose -f $(COMPOSE_FILE) up --build
 
 .PHONY: up down build re clean
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,12 +1,9 @@
-mkdir -p /var/www/
-mkdir -p /var/www/wordpress/
 
 cd /var/www/wordpress/
-
 wp core download  --allow-root
 
 wp config create --dbname=${MYSQL_DATABASE} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=${WORDPRESS_DB_HOST} --path=/var/www/wordpress/ --allow-root
 
-wp core install --url=${DOMAIN_NAME} --title=MyWebsite --admin_user=lolo --admin_password=123456 --admin_email=lolo@tee.com --allow-root
-wp user create teek teek@email.com --user_pass=1234 --role=author --allow-root
+wp core install --url=${DOMAIN_NAME} --title=MyWebsite --admin_user=${WORDPRESS_ROOT_USER} --admin_password=${WORDPRESS_ROOT_PASSWORD} --admin_email=lolo@tee.com --allow-root
+wp user create ${WORDPRESS_USER} teek@email.com --user_pass=${WORDPRESS_PASSWORD} --role=author --allow-root
 exec $@
